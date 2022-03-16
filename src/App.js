9f8aa80c6b3bb5b587535ae4dce3c4bbf5d71a59
mdmehedihasan212 <mdmehedihasan384@gmail.com>
2022-03-16 13:51:22 +0600

    add some style in div

--- src/App.js
+++ src/App.js
@@ -1,47 +1,58 @@
 import logo from './logo.svg';
 import './App.css';
 
-const country = 'bangladesh';
-const singer = { name: 'james', job: 'singer', catagory: 'band' }
-const singerStyle = {
-  color: 'blue',
-  backgroundColor: 'white'
-}
-const singerStyle2 = {
-  color: 'gold',
-  backgroundColor: 'red'
-
+const friendsStyle = {
+  color: 'goldenrod',
+  backgroundColor: 'blue',
+  border: '3px solid black',
+  borderRadius: '20px',
+  padding: '20px',
+  margin: '20px'
 }
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <div className="container">
-          <h1>Hello React</h1>
-          <p>My Self Md Mehedi Hasan</p>
-        </div>
-        <div className="music">
-          <p style={singerStyle}>Country: {country}</p>
-          <p style={{ color: 'red', backgroundColor: 'yellow' }}>Singer: {singer.name}</p>
-          <p style={singerStyle2}>Job: {singer.job}</p>
-          <p style={{ color: 'white', backgroundColor: 'blue' }}>Catagory: {singer.catagory}</p>
-        </div>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Person></Person>
+      <Person></Person>
+      <Person></Person>
+      <Family></Family>
+      <Family></Family>
+      <Family></Family>
+      <Friends></Friends>
+      <Friends></Friends>
+      <Friends></Friends>
+
     </div>
   );
 }
 
 export default App;
+
+function Person() {
+  return (
+    <div className="personStyle">
+      <h1>Md Mehedi Hasan</h1>
+      <p>This Time Is My Life Is Verry Bad Momment But I don't Tention Because Allah Alaways Save Me Any Problem In My Life Its Belive Me</p>
+    </div>
+  );
+
+}
+
+function Family() {
+  return (
+    <div style={{ color: 'white', backgroundColor: 'goldenrod', border: '3px solid red', margin: '20px', borderRadius: '20px', padding: '20px' }}>
+      <h1>I Have A Smile Family</h1>
+      <p>Sami Is My Son And Samiya Is My Lovely Wife And My Mother Is My Heart And My MaMa Is Too Good</p>
+    </div>
+  )
+}
+
+function Friends() {
+  return (
+    <div style={friendsStyle}>
+      <h1>I Have Some Good Friends</h1>
+      <p>Monir And Bahadur And Imran Are Goods Friend My Lost Life Time</p>
+    </div>
+  )
+}
