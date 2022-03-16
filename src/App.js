0d5ac2630b69a3cc58f8760fee3f9003150b002d
mdmehedihasan212 <mdmehedihasan384@gmail.com>
2022-03-16 12:38:22 +0600

    add some backgroundColor

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 
+const country = 'bangladesh';
+const singer = { name: 'james', job: 'singer', catagory: 'band' }
+const singerStyle = {
+  color: 'blue',
+  backgroundColor: 'white'
+}
+const singerStyle2 = {
+  color: 'gold',
+  backgroundColor: 'red'
+
+}
+
 function App() {
   return (
     <div className="App">
@@ -8,6 +20,13 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <div className="container">
           <h1>Hello React</h1>
+          <p>My Self Md Mehedi Hasan</p>
+        </div>
+        <div className="music">
+          <p style={singerStyle}>Country: {country}</p>
+          <p style={{ color: 'red', backgroundColor: 'yellow' }}>Singer: {singer.name}</p>
+          <p style={singerStyle2}>Job: {singer.job}</p>
+          <p style={{ color: 'white', backgroundColor: 'blue' }}>Catagory: {singer.catagory}</p>
         </div>
         <p>
           Edit <code>src/App.js</code> and save to reload.
